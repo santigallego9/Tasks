ba8087e0b4fad2b697adb0f3b4b9009e7748fa8c
Santi Gallego <santigallego9@gmail.com>
2016-06-10 17:53:14 -0500

    Santi 6/11/2016

--- app/src/main/java/edu/stanford/cs193a/sgalleg9/tasks/MainActivity.java
+++ app/src/main/java/edu/stanford/cs193a/sgalleg9/tasks/MainActivity.java
@@ -2,6 +2,8 @@ package edu.stanford.cs193a.sgalleg9.tasks;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -12,6 +14,7 @@ import stanford.androidlib.*;
 public class MainActivity extends SimpleActivity {
 
     private ArrayList<String> items;
+    private EditText addText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +23,49 @@ public class MainActivity extends SimpleActivity {
 
         ListView list = find(R.id.items_list);
 
+
         items = new ArrayList<String>();
         list.setOnItemLongClickListener(this);
         list.setOnItemClickListener(this);
     }
 
     public void addButtonClicked(View view) {
-        EditText addText = find(R.id.add_text);
+        addText = find(R.id.add_text);
         String item = addText.getText().toString();
 
         items.add(item);
 
         SimpleList.with(this).setItems(findListView(R.id.items_list), items);
 
+        addText = find(R.id.add_text);
         addText.setText("");
     }
 
     @Override
     public void onItemClick(ListView list, int index) {
+        String holder = items.get(index);
+        items.remove(index);
+        items.add(holder);
+
+        setList();
+
         toast("TAP: " + index);
     }
 
     @Override
     public boolean onItemLongClick(ListView list, int index) {
+        items.remove(index);
+
         toast("LONG: " + index);
+
+        setList();
         return true;
+    }
+
+    public void setList() {
+        SimpleList.with(this).setItems(findListView(R.id.items_list), items);
+
+        addText = find(R.id.add_text);
+        addText.setText("");
     }
 }
